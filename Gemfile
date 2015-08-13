f7cac4891ed048d01ba35fb674bcdfbc0a74b74a
Codice-e-Bella <codiceebella@gmail.com>
2015-08-13 10:19:57 -0500

    Added Image upload, paperclip, edit and delete

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'simple_form', '~> 3.0.2'
 gem 'haml', '~> 4.0.5'
 gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'devise', '~> 3.5.2'
+gem 'paperclip', '~> 4.3'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,7 @@ Rails.application.configure do
 
   #Needed for devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # Needed for imagestick
+  Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin'
+
 end
